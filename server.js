c568fe7695fb6b627c6e3676448db9ca1d932586
David White <dwhite0627@gmail.com>
2020-03-21 22:09:42 -0400

    Add initial data object

--- server.js
+++ server.js
@@ -6,8 +6,20 @@ server.use(express.json());
 server.use(cors()); // For the stretch
 const port = 5000;
 
-// Request handlers
-server.get('/')
+// Initial data state
+const data = {
+  users: [
+    {
+      id: "1", // hint: use the shortid npm package to generate it
+      name: "Jane Doe", // String, required
+      bio: "Not Tarzan's Wife, another Jane" // String, required
+    }
+  ]
+}
+
+// Request handling, utils
+const reqHandler = require('./serverRequests.js');
+const utils = require("./utils/utils");
 
 // Listen to port
 server.listen(port, () => {
