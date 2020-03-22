4e25845e9e299d43ab2cd79a7ae9a902cda394a5
David White <dwhite0627@gmail.com>
2020-03-21 22:10:06 -0400

    Add the functionality for the API to accept POST requests to add a user

--- server.js
+++ server.js
@@ -21,6 +21,35 @@ const data = {
 const reqHandler = require('./serverRequests.js');
 const utils = require("./utils/utils");
 
+// Add user
+server.post("/api/users", (req, res) => {
+  const actualFields = Object.keys(req.body);
+  const required = ['id', 'name', 'bio'];
+  const missingFields = utils.validateHasFields(required, actualFields); // check for missing fields, if any
+  // If there are missing fields, alert the user and send back a 400 code
+  if (missingFields.length > 0) {
+    const missingString = utils.formatFieldString(missingFields);
+    res
+      .status(400)
+      .json({
+        errorMessage: "Please provide the " + missingString + " for the user"
+      });
+  } else {
+    // Attempt to save the data
+    try {
+      data.users.push(req.body); // Add to the data object in this project
+      res.status(201).json(req.body); // Let user know success by sending back the object added
+    } catch (err) {
+      // If the save attempt fails, alert the user
+      res
+        .status(500)
+        .json({
+          errorMessage:
+            "There was an error while saving the user to the database"
+        }); // notify user of the unexpected failure to save
+    }
+  }
+});
 // Listen to port
 server.listen(port, () => {
   console.log(`Server running on port ${port}`)
