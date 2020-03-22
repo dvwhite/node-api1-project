a89301030c6d36d0280e46f04e97e4315b77ec42
David White <dwhite0627@gmail.com>
2020-03-22 09:37:19 -0400

    Clean up if/else blocks, remove file of custom handler functions

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Create and configure the server
-const express = require('express');
+const express = require("express");
 const server = express();
-const cors = require('cors');
+const cors = require("cors");
 server.use(express.json());
 server.use(cors()); // For the stretch
 const port = 5000;
@@ -15,39 +15,35 @@ const data = {
       bio: "Not Tarzan's Wife, another Jane" // String, required
     }
   ]
-}
+};
 
 // Request handling, utils
-const reqHandler = require('./serverRequests.js');
 const utils = require("./utils/utils");
 
 // Add user
 server.post("/api/users", (req, res) => {
+  // Check for missing fields, if any
   const actualFields = Object.keys(req.body);
-  const required = ['id', 'name', 'bio'];
-  const missingFields = utils.validateHasFields(required, actualFields); // check for missing fields, if any
+  const required = ["id", "name", "bio"];
+  const missingFields = utils.validateHasFields(required, actualFields); 
   // If there are missing fields, alert the user and send back a 400 code
   if (missingFields.length > 0) {
     const missingString = utils.formatFieldString(missingFields);
-    res
-      .status(400)
-      .json({
-        errorMessage: "Please provide the " + missingString + " for the user"
-      });
-  } else {
-    // Attempt to save the data
-    try {
-      data.users.push(req.body); // Add to the data object in this project
-      res.status(201).json(req.body); // Let user know success by sending back the object added
-    } catch (err) {
-      // If the save attempt fails, alert the user
-      res
-        .status(500)
-        .json({
-          errorMessage:
-            "There was an error while saving the user to the database"
-        }); // notify user of the unexpected failure to save
-    }
+    res.status(400).json({
+      errorMessage: "Please provide the " + missingString + " for the user"
+    });
+    return;
+  } 
+  
+  // Attempt to save the data
+  try {
+    data.users.push(req.body); // Add to the data object in this project
+    res.status(201).json(req.body); // Let user know success
+  } catch (err) {
+    // If the save attempt fails, alert the user
+    res.status(500).json({
+      errorMessage: "There was an error while saving the user to the database"
+    }); // notify user of the unexpected failure to save
   }
 });
 
@@ -56,53 +52,56 @@ server.get("/api/users", (req, res) => {
   try {
     res.status(200).json(data.users);
   } catch (err) {
-    res
-      .status(500)
-      .json({
-        errorMessage: "The users information could not be retrieved."
-      });
+    res.status(500).json({
+      errorMessage: "The users information could not be retrieved."
+    });
   }
 });
 
 // Get one user
-server.get('/api/users/:id', (req, res) => {
+server.get("/api/users/:id", (req, res) => {
   // Check if the user with the specified is found, and if not, send back a 404
-  console.log("id:", req.body.id, typeof(req.params.id))
-  const user = data.users.find(item => String(item.id) === String(req.params.id));
-  if (!user) {
+  const user = data.users.find(
+    item => String(item.id) === String(req.params.id)
+  );
+  if (user) {
+    // Notify user if there's an error in retrieving the user from the database
+    try {
+      res.status(200).json(user);
+    } catch (err) {
+      res
+        .status(500)
+        .json({ errorMessage: "The user information could not be retrieved." });
+    }
+  } else {
     res
-      .status(404)
-      .json({ message: "The user with the specified ID does not exist." });
-  }
-
-  // Notify user if there's an error in retrieving the user from the database
-  try {
-    res.status(200).json(user);
-  } catch (err) {
-    res
-      .status(500)
-      .json({ errorMessage: "The user information could not be retrieved." });
+    .status(404)
+    .json({ message: "The user with the specified ID does not exist." });
   }
 });
 
 // Delete user
-server.delete('/api/users/:id', (req, res) => {
+server.delete("/api/users/:id", (req, res) => {
   // Check if the user with the specified is found, and if not, send back a 404
-  const user = data.users.find(item => String(item.id) === String(req.params.id));
-  if (!user) {
-    res
-      .status(404)
-      .json({ message: "The user with the specified ID does not exist." });
-  } else {
+  const user = data.users.find(
+    item => String(item.id) === String(req.params.id)
+  );
+  if (user) {
     // Notify user if there's an error in retrieving the user from the database
     try {
-      data.users = data.users.filter(item => String(item.id) !== String(req.params.id));
+      data.users = data.users.filter(
+        item => String(item.id) !== String(req.params.id)
+      );
       res.status(200).json(data.users);
     } catch (err) {
       res
         .status(500)
         .json({ errorMessage: "The user information could not be retrieved." });
     }
+  } else {
+    res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exist." });
   }
 });
 
@@ -147,5 +146,5 @@ server.put("/api/users/:id", (req, res) => {
 
 // Listen to port
 server.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`);
 });
