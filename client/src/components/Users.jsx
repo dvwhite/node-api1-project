994bbf41dfe3bb0d6879fe8343984a8b998628f2
David White <dwhite0627@gmail.com>
2020-04-06 20:54:16 -0400

    Add the Users component
    ## A flex container for an array of User components

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -2,35 +2,48 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 // Component imports
-import User from './User';
+import User from "./User";
+import AddUser from "./AddUser";
 
 // Styled components
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
   flex-flow: row wrap;
   width: 100%;
-`
+`;
 
 const Users = () => {
   const [users, setUsers] = useState([]);
+
+  // Helpers
+  const addToUsers = (newUser) => {
+    setUsers([...users, newUser]);
+  };
+
+  const removeUser = (id) => {
+    setUsers([...users.filter((user) => Number(user.id) !== Number(id))]);
+  };
 
   // Get the users from the API backend
   useEffect(() => {
     axios
       .get("http://localhost:5000/api/users")
       .then((res) => {
-        console.log(res.data);
+        console.log("Users:", res.data);
         setUsers(res.data);
       })
       .catch((err) => console.error(err.response));
   }, []);
 
   return (
-    <>
-      {users?.map(user => <User data={user} />)}
-    </>
+    <Wrapper>
+      {users?.map((user, idx) => (
+        <User data={user} removeUser={removeUser} key={idx} />
+      ))}
+      <AddUser addToUsers={addToUsers} />
+    </Wrapper>
   );
 };
 
